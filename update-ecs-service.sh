ac5f654dccec6bd4b36b7fcd88607837b4b959e3
Gerald Alinio <38755420+imgeraldalinio@users.noreply.github.com>
2023-09-02 09:46:47 +0800

    Update update-ecs-service.sh

--- update-ecs-service.sh
+++ update-ecs-service.sh
@@ -1,15 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
-# Run this script after enabling the ECS exec command: ./ecs-update.sh qa-helloworld
-CLUSTER_NAME=$1
-SERVICE_NAME=$CLUSTER_NAME
-REGION=us-east-1
-AWS_PROFILE=$2
-PROFILE="${AWS_PROFILE:-default}"  # If variable not set or null, use default.
+# Usage: ./ecs-update.sh <CLUSTER_NAME> [SERVICE_NAME] [AWS_PROFILE]
 
-if ! grep -rw "$PROFILE" ~/.aws/ > /dev/null 2>&1; then
-  echo "AWS_PROFILE not found"
+# Validate input parameters
+if [ $# -lt 1 ]; then
+  echo "Usage: $0 <CLUSTER_NAME> [SERVICE_NAME] [AWS_PROFILE]"
   exit 1
 fi
 
-aws ecs update-service --cluster $CLUSTER_NAME --service $SERVICE_NAME --region $REGION --enable-execute-command --force-new-deployment --profile $PROFILE
+CLUSTER_NAME="$1"
+SERVICE_NAME="${2:-$CLUSTER_NAME}"
+REGION="us-east-1"
+AWS_PROFILE="${3:-default}"
+
+# Check if the AWS_PROFILE exists in the AWS configuration
+if ! aws configure list-profiles | grep -q "^$AWS_PROFILE$"; then
+  echo "AWS_PROFILE '$AWS_PROFILE' not found in your AWS configuration."
+  exit 1
+fi
+
+# Enable ECS Exec command and force a new deployment
+aws ecs update-service \
+  --cluster "$CLUSTER_NAME" \
+  --service "$SERVICE_NAME" \
+  --region "$REGION" \
+  --enable-execute-command \
+  --force-new-deployment \
+  --profile "$AWS_PROFILE"
+
+echo "ECS Exec command enabled for service '$SERVICE_NAME' in cluster '$CLUSTER_NAME'."
